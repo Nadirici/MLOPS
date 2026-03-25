47df5303cd384308199fbb6bd067ab66a2f72329
Nadir NEHILI <nadir1401@gmail.com>
2026-03-25 22:16:05 +0100

    Integrated all pipelines

---
+++ tests/pipelines/processing/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from purchase_predict.pipelines.loading.nodes import load_csv_from_bucket
+from purchase_predict.pipelines.processing.nodes import encode_features
+from kedro.io import DataCatalog, MemoryDataset
+
+
+@pytest.fixture(scope="module")
+def project_id():
+    return "MLOPS"
+
+
+@pytest.fixture(scope="module")
+def primary_folder():
+    return "tp_mlops_nadir//data-test.csv"
+
+
+@pytest.fixture(scope="module")
+def dataset_not_encoded(project_id, primary_folder):
+    return load_csv_from_bucket(project_id, primary_folder)
+
+
+@pytest.fixture(scope="module")
+def test_ratio():
+    return 0.3
+
+
+@pytest.fixture(scope="module")
+def dataset_encoded(dataset_not_encoded):
+    return encode_features(dataset_not_encoded)["features"]
+
+
+@pytest.fixture(scope="module")
+def catalog_test(dataset_not_encoded, test_ratio):
+    catalog = DataCatalog(
+        {"primary": MemoryDataset(dataset_not_encoded), "params:test_ratio": MemoryDataset(test_ratio)}
+    )
+    return catalog
